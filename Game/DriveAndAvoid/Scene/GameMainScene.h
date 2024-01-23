86c5779d80ad7ae35a580340593fb3cdcb496836
SUNEO33 <rikuto-kawata-k23f@stu.kbc.ac.jp>
2024-01-23 11:10:50 +0900

    途中
    途中

--- Game/DriveAndAvoid/Scene/GameMainScene.h
+++ Game/DriveAndAvoid/Scene/GameMainScene.h
@@ -1,5 +1,19 @@
 #pragma once
-class GameMainScene
+
+#include "SceneBase.h"
+#include "../Object/Player.h"
+#include "../Object/Enemy.h"
+
+class GameMainScene:public SceneBase
 {
+private:
+	int high score;      //ハイスコア
+	int back_ground;     //背景画像
+	int barrier_image;    //バリア画像
+	int mileage;          //走行距離
+	int enemy_image[3];   //敵画像
+	int enemy_count[3];   //通り過ぎた敵カウント
+	Player* player;       //プレイヤー
+	Enemy** enemy;        //敵
 };
 
