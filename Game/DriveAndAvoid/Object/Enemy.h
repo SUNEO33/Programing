01000ef2688a3d7d398eae7c945082025b9599a4
SUNEO33 <rikuto-kawata-k23f@stu.kbc.ac.jp>
2024-02-08 13:16:43 +0900

    完成
    完成

--- Game/DriveAndAvoid/Object/Enemy.h
+++ Game/DriveAndAvoid/Object/Enemy.h
@@ -11,6 +11,7 @@ private:
 	Vector2D location;    //位置情報
 	Vector2D box_size;    //当たり判定の大きさ
 
+
 public:
 	Enemy(int type, int handle);
 	~Enemy();
@@ -23,6 +24,6 @@ public:
 	int GetType() const;      //タイプ取得
 	Vector2D GetLocation() const;    //位置情報の取得
 	Vector2D GetBoxSize() const;     //当たり判定の大きさを取得
-
+	
 };
 
